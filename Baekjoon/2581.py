bb918cc06c3954fc64132545bc22f8f1d0222d63
H2W0N <woohw97@naver.com>
2022-08-04 16:11:24 +0900

    에라토스테네스의 체, 소수

--- Baekjoon/2581.py
+++ Baekjoon/2581.py
@@ -1,4 +1,6 @@
 # 소수
+
+# 에라토스테네스의 체
 def prime_list(n):
     # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
     sieve = [True] * n
@@ -12,11 +14,42 @@ def prime_list(n):
     # 소수 목록 산출
     return [i for i in range(2, n) if sieve[i] == True]
 
-m, n = map(int, input().split())
-l1 = prime_list(m)
-l2 = prime_list(n)
-print(l1)
-print(l2)
-for i in range(len(l1)) :
-    l2.remove(l1[i])
-print(l2)
+# 나의 풀이
+for i in range(m,n+1) :
+    if i==1 :
+        pass
+    elif i==2 :
+        arr.append(i)
+    else :
+        for j in range(2,i) :
+            if i%j==0 :
+                break
+            elif j==i-1 :
+                arr.append(i)
+if len(arr)==0 :
+    print(-1)
+else :
+    print(sum(arr))
+    print(min(arr))
+
+
+start_num = int(input())
+last_num = int(input())
+
+# 다른 사람의 풀이
+sosu_list = []
+for num in range(start_num, last_num+1):
+    error = 0
+    if num > 1 :
+        for i in range(2, num):  # 2부터 num-1까지
+            if num % i == 0:
+                error += 1
+                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
+        if error == 0:
+            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
+            
+if len(sosu_list) > 0 :
+    print(sum(sosu_list))
+    print(min(sosu_list))
+else:
+    print(-1)
